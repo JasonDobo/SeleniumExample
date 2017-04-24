c6300a3a5231b4e4df1178620e83bbae73f7f323
Jason Dobo <jason.dobo@moonpig.com>
2017-04-24 16:49:06 +0100

    Simple pom file to import selenium and unit dependancies

---
+++ src/main/java/Main.java
@@ -0,0 +1,5 @@
+/**
+ * Created by jason.dobo on 24/04/2017.
+ */
+public class Main {
+}
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,26 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+/**
+ * Created by jason.dobo on 24/04/2017.
+ */
+public class MainTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testOne() {
+
+    }
+
+}
