6496808b250d1572d9dfa29ea8943d3d87acb2ac
Jason Dobo <jason.dobo@moonpig.com>
2017-04-26 17:39:44 +0100

    Added test scenarios for web site

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
 # SeleniumExample
-# Simple selenium example project with Maven
 
-https://sites.google.com/a/chromium.org/chromedriver/downloads
+This project was developed using Selenium with Java using Intellij.
+
+## Setup:
+1. Download the Selenium Chromedriver and extract the binary (https://sites.google.com/a/chromium.org/chromedriver/downloads)
+2. Install and launch Intellij
+3. With Intellij open the SeleniumExample project
+3. Open the Maven view and select "Reimport all Maven projects"
+4. Open MainTest.java and update line 22 to contain the locaiton of the chromedriver binary
+
+## Running:
+- Navigate to the "MainTest.java" file in intellij and select run 'MainTest'
+or
+- Navigate to the "MainTest.java" file in intellij and press CMD + Shift + R
+
+## Notes:
+### Predefined acceptance criteria
+- testSuccessfulSearchForProducts
+- testUnSuccessfulSearchForProducts
+Cover the 2 predefined acceptance criteria
+
+## Additional acceptance criteria
+-
+-
+Cover the 2 additional acceptance criteria
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -12,10 +12,6 @@ import org.openqa.selenium.support.ui.Wait;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-
-/**
- * Created by jason.dobo on 24/04/2017.
- */
 public class MainTest {
 
     WebDriver driver;
@@ -23,8 +19,11 @@ public class MainTest {
 
     @Before
     public void setUp() throws Exception {
-        System.setProperty("webdriver.chrome.driver", "/Users/jasondobo/Downloads/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "/Users/jason.dobo/Downloads/chromedriver");
         driver = new ChromeDriver();
+
+        driver.get("https://www.moo.com/uk/");
+        handleNewsLetterOverlay();
     }
 
     @After
@@ -32,52 +31,100 @@ public class MainTest {
         driver.quit();
     }
 
+    // 1. When a customer searches for a valid product on the website, they should see a view of
+    // products matching that search term - example: business cards
     @Test
-    public void testOne() throws InterruptedException {
-        driver.get("https://www.moo.com/uk/");
-        handleNewsLetterOverlay();
+    public void testSuccessfulSearchForProducts() {
+        enterSearchString("business cards");
+        pressSearchButton();
 
-        WebElement searchBar = waitForElement(By.id("query"), DEFAULT_TIMEOUT);
-        searchBar.click();
-        searchBar.sendKeys("business cards");
-
-        WebElement searchButton = waitForElement(By.className("search__btn"), DEFAULT_TIMEOUT);
-//        WebElement searchButton = driver.findElement(By.cssSelector("input[class='search__btn u-p0 js-search-btn']"));
-        searchButton.click();
-
-        List<WebElement> jaso = driver.findElements(By.className("close"));
-
-        String attribute = jaso.get(0).getAttribute("data-webdriver-automation-id");
-        jaso.get(0).click();
-
-        WebElement jason = driver.findElement(By.id("newsletter-overlay-close-link"));
-        waitForElement(By.id("newsletter-overlay-close-link"), 30);
-
-
-
-        fluentWait(10).until(ExpectedConditions.visibilityOfElementLocated(By.id("newsletter-overlay-close-link")));
-
-
-
-        System.out.println("Successfully opened the website www.google.com");
-        WebElement element = driver.findElement(By.id("lst-ib"));
-        Assert.assertTrue(element.isDisplayed());
-        element.sendKeys("Test");
-        element.sendKeys(Keys.RETURN);
-
-
-        Wait wait = new FluentWait(driver)
-                .withTimeout(30, TimeUnit.SECONDS)
-                .pollingEvery(5, TimeUnit.SECONDS)
-                .ignoring(NoSuchElementException.class);
-
-        ExpectedCondition<WebElement> elementExpectedCondition  = ExpectedConditions.visibilityOfElementLocated(By.id("lst-ib"));
-        wait.until(elementExpectedCondition);
-
-        Thread.sleep(5);
-        driver.quit();
+        waitForVisibilityOfElement(By.className("gsc-results"));
+        waitForInvisibilityOfElement(By.className("gs-no-results-result"));
+        List<WebElement> results = driver.findElements(By.className("gsc-result"));
+        Assert.assertTrue(results.size() > 0);
     }
 
+    // 2. When a customer searches for an invalid product on the website, they should receive a
+    // message telling them - example: sdjfnjsdfj
+    @Test
+    public void testUnSuccessfulSearchForProducts() {
+        enterSearchString("sdjfnjsdfj");
+        pressSearchButton();
+
+        waitForVisibilityOfElement(By.className("gs-no-results-result"));
+        waitForInvisibilityOfElement(By.className("gsc-results"));
+    }
+
+    // 3. When a customers logs in with valid credentials on the web site, then the your account screen is displayed
+    // In practise you should be using a test environment and not create test accounts on live
+    @Test
+    public void testSignInWithValidCredentials() {
+        pressAccountIcon();
+        WebElement label = waitForVisibilityOfElement(By.id("fb-label"));
+        Assert.assertTrue(label.getText().equalsIgnoreCase("Sign in with"));
+
+        enterUserSignInCredentials("jason.dobo@gmail.com", "qwerty123");
+        pressSignInButton();
+
+        waitForVisibilityOfElement(By.linkText("Your Account"));
+    }
+
+    // 3. When a customers is on the sign page, then they can choose to create a account and enter details
+    // In practise you should be using a test environment and not create test accounts on live
+    @Test
+    public void testCreateAnAccount() {
+
+    }
+
+    // Action helpers
+    private void handleNewsLetterOverlay() {
+        tryWaitForElement(ExpectedConditions.elementToBeClickable(By.className("close")), 30);
+        List<WebElement> closeButtons = driver.findElements(By.className("close"));
+
+        for (WebElement element : closeButtons) {
+            String attribute = element.getAttribute("data-webdriver-automation-id");
+            if (attribute.equalsIgnoreCase("newsletter-overlay-close-link")) {
+                System.out.println("DEBUG: About to dismiss news letter overlay");
+                element.click();
+
+                Boolean close = waitForInvisibilityOfElement(By.className("close"));
+                Assert.assertTrue(close);
+                break;
+            }
+        }
+    }
+
+    private void enterSearchString(String criteria) {
+        WebElement searchBar = waitToBeClickableForElement(By.id("query"));
+
+        searchBar.click();
+        searchBar.sendKeys(criteria);
+    }
+
+    private void enterUserSignInCredentials(String email, String password) {
+        WebElement emailField = waitForVisibilityOfElement(By.id("txtEmailSignIn"));
+        WebElement passwordField = waitForVisibilityOfElement(By.id("txtPasswordSignIn"));
+
+        emailField.sendKeys(email);
+        passwordField.sendKeys(password);
+    }
+
+    private void pressSignInButton() {
+        WebElement signInButton = waitToBeClickableForElement(By.id("btnLogin"));
+        signInButton.click();
+    }
+
+    private void pressSearchButton() {
+        WebElement searchButton = waitForVisibilityOfElement(By.className("search__btn"));
+        searchButton.click();
+    }
+
+    private void pressAccountIcon() {
+        WebElement account = waitToBeClickableForElement(By.className("js-nav__link--account"));
+        account.click();
+    }
+
+    // Element finders and checkers
     private Wait<WebDriver> fluentWait(long timeout) {
         return new FluentWait<>(driver)
                 .withTimeout(timeout, TimeUnit.SECONDS)
@@ -86,40 +133,50 @@ public class MainTest {
                 .ignoring(NoSuchElementException.class);
     }
 
-    private WebElement waitForElement(By by, int seconds) {
-        WebElement element;
-        try {
-            element = fluentWait(seconds).until(ExpectedConditions.visibilityOfElementLocated(by));
-        } catch (TimeoutException e) {
-            System.out.println("Try find element " + by.toString() + ", not found");
+    private WebElement waitForVisibilityOfElement(By by) {
+        WebElement element = tryWaitForElement(ExpectedConditions.visibilityOfElementLocated(by), DEFAULT_TIMEOUT);
+
+        if (element == null) {
+            System.out.println("Waited for visibility of element by " + by.toString() + ", not found");
             throw new NoSuchElementException(by.toString());
+        } else {
+            return element;
         }
-
-        return element;
     }
 
-    private WebElement tryWaitForElement(By by, int seconds) {
+    private WebElement waitToBeClickableForElement(By by) {
+        WebElement element = tryWaitForElement(ExpectedConditions.elementToBeClickable(by), DEFAULT_TIMEOUT);
+
+        if (element == null) {
+            System.out.println("Waited for clickable of element by " + by.toString() + ", not found");
+            throw new NoSuchElementException(by.toString());
+        } else {
+            return element;
+        }
+    }
+
+    private WebElement tryWaitForElement(ExpectedCondition<WebElement> condition, int seconds) {
         WebElement element = null;
+
         try {
-            element = fluentWait(seconds).until(ExpectedConditions.visibilityOfElementLocated(by));
+            element = fluentWait(seconds).until(condition);
         } catch (TimeoutException e) {
-            System.out.println("Try find element " + by.toString() + ", not found");
+            System.out.println("Try find element " + condition.toString() + ", not found");
         }
 
         return element;
     }
-    private void handleNewsLetterOverlay() {
-        tryWaitForElement(By.className("close"), 30);
-        List<WebElement> closeButtons = driver.findElements(By.className("close"));
 
-        for (WebElement element : closeButtons) {
-            String attribute = element.getAttribute("data-webdriver-automation-id");
-            if (attribute.equalsIgnoreCase("newsletter-overlay-close-link")) {
-                System.out.println("DEBUG: About to dismiss news letter overlay");
-                element.click();
-                break;
-            }
+    private boolean waitForInvisibilityOfElement(By by) {
+        int seconds = DEFAULT_TIMEOUT;
+        boolean result = false;
+
+        try {
+            result = fluentWait(seconds).until(ExpectedConditions.invisibilityOfElementLocated(by));
+        } catch (TimeoutException e) {
+            System.out.println("Try find result " + by.toString() + ", not found");
         }
-    }
 
+        return result;
+    }
 }
